171f2a3e0dcb63758eb20f662bf587f998d1b1d6
Shaolun Hong <4412593+Shaolun5@users.noreply.github.com>
2021-10-08 15:17:25 +0200

    fix for case 3

--- assignment3/exercise3/exercise3_case3.cpp
+++ assignment3/exercise3/exercise3_case3.cpp
@@ -2,15 +2,9 @@
 # include <omp.h>
 # include <sys/time.h>
 
-/**
- * Use the OMP NUM THREADS environment variable to control the number of threads
- * Example: OMP_NUM_THREADS=2 ./case1.exe N
- * @param program
- */
-
 void show_help_info(char *program){
-    std::cout << "Usage: " << program << "N" << std::endl;
-    std::cout << "N: the size of array (int, N > 0)"<< std::endl;
+    std::cout << "Usage: " << program << " T" << std::endl;
+    std::cout << "T: the number of threads (int, T > 0)"<< std::endl;
     exit(-1);
 }
 
@@ -33,20 +27,29 @@ static void free_array(int ** array, int N) {
     for (i = 0 ; i < N ; i++)
         free(array[i]);
     free(array);
-
 }
+
+void print_matrix(int** c, int dim){
+    std::cout <<"Matrix:" << std::endl;
+    for(int i = 0; i < dim; ++i){
+        for(int j = 0; j < dim; ++j)
+            std::cout << c[i][j]<<" ";
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char * argv[]){
 
-    int threads_number = 0; // the number of threads
+    int threads_number; // the number of threads
     int ** a, ** b, ** c; //arrays
-    int dim; // the length of the array
+    int dim = 40; // the length of the array
     double time;			//variables for timing
     struct timeval ts,tf;
 
     if (argc != 2){
         show_help_info(argv[0]);
     } else{
-        dim = std::stoi(argv[1]);
+        threads_number = std::stoi(argv[1]);
     }
 
     a = allocate_array(dim);
@@ -58,21 +61,27 @@ int main(int argc, char * argv[]){
 
     gettimeofday(&ts,NULL);
 
-    # pragma omp parallel default (none) shared(a,b,c,dim)
+    #pragma omp parallel default (none) shared(a,b,c,dim) num_threads(threads_number)
     {
         // case 3: all loops are parallelized
         # pragma omp for collapse(3)
         for (int i = 0; i < dim; i++){
             for (int j = 0; j < dim; j++){
                 for (int k = 0; k < dim; k++){
+                    //prevent multi-threads writing to c[i][j] at the same time
+                    #pragma omp atomic
                     c[i][j] += a[i][k] * b[k][j];
                 }
             }
         }
     }
-
     gettimeofday(&tf,NULL);
     time = (tf.tv_sec-ts.tv_sec)+(tf.tv_usec-ts.tv_usec)*0.000001;
+    
+    print_matrix(a, dim);
+    print_matrix(b, dim);
+    print_matrix(c, dim);
+    
     free_array(a, dim);
     free_array(b, dim);
     free_array(c, dim);
