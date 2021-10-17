155f58b152d3500b4f3d90a9711c27409c363c38
zezhong zheng <1065709724@qq.com>
2021-10-17 08:43:28 +0200

    add ex1-1;

--- assignment4/exercise1.cpp
+++ assignment4/exercise1.cpp
@@ -2,18 +2,17 @@
 #include <cmath>
 #include <cstring>
 #include <chrono>
-#include <omp.h>
+#include <mpi.h>
 
 // marking the primes in this array, prime==true
 bool* flags;
-
 int sqrt_max;
+#define MAX_SIZE 100
 
 void show_help_info(char *program){
-    std::cout << "Usage: " << program << "T N" << std::endl;
-    std::cout << "T: the number of threads (int, T > 0)"<< std::endl;
+    std::cout << "Usage: " << program << "N" << std::endl;
     std::cout << "N: the maximum positive interger (int, N >= 1)"<< std::endl;
-    exit(1);
+    exit(-1);
 }
 
 
@@ -28,68 +27,115 @@ void eratosthenes(int lowerbound, int upperbound) {
     }
 }
 
+
+void eratosthenes_worker(int lowerbound, int upperbound) {
+    for (int i = lowerbound; i <= upperbound; ++i) {
+        for(int q = 2; q <= sqrt_max; ++q){
+        	if(flags[q] && i % q == 0){
+        		flags[i] = false;
+        		break;
+        	}
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3){
+    if (argc != 2){
         show_help_info(argv[0]);
     }
 
-    // load and check the params
-    int threads;    
     int maximum;
     try{
-        // the number of threads, excluding main thread
-        threads = std::stoi(argv[1]);
-        maximum = std::stoi(argv[2]);
+        maximum = std::stoi(argv[1]);
     } catch(std::exception) {
         show_help_info(argv[0]);
     }
 
-    if (threads < 1 || maximum < 1){
+    if (maximum < 1){
         show_help_info(argv[0]);
     }
+
+    char name[MAX_SIZE]; // name of processor
+    int rank, pop, p, len;
+
+    /* no MPI calls before this point */
+    MPI_Init(&argc, &argv);
+    // the rank of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &pop);
 
     flags = new bool[maximum + 1];
     memset(flags, true, sizeof(bool) * (maximum + 1));
     flags[0] = flags[1] = false; // exclude 0 and 1
-
     sqrt_max = (int) std::sqrt(maximum); // round down
 
-    // ============== multi-threads part ================
     int lower_bound = sqrt_max + 1;
     int upper_bound = maximum;
+
+    int total = upper_bound - lower_bound + 1;
+    int chunk = total / pop;
+    int remainder = total - chunk * pop;
+
+    // define msg for each processor
+    bool msg = new bool[chunk+1];
+    int index = lower_bound;
 
     // *** timing begins here ***
     auto start_time = std::chrono::system_clock::now();
 	// sequentially compute primes up to sqrt_max
     eratosthenes(2, sqrt_max); 
-    
-    // set the number of threads
-    omp_set_num_threads(threads);
-    #pragma omp parallel for
-    for(int i = lower_bound; i <= upper_bound; ++i){
-        // check if it's prime
-        for(int q = 2; q <= sqrt_max; ++q){
-            if(flags[q] && i % q == 0){
-                flags[i] = false;
-                break;
+
+    // processor 0, receive msg from processors
+    if (rank == 0){
+        MPI_Get_processor_name(name, &len);
+        printf("%s receive from process %d of %d\n", name, rank, pop);
+        // receive from other processors
+        for (p = 1; p < pop; p++){
+            MPI_Recv(msg, strlen(msg)+1, MPI_CXX_BOOL, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // combine msg
+            for (int i = 0; i < strlen(msg); i++){
+                flags[index++] = msg[i];
             }
         }
+
+    } else{
+        // calculate and send msg
+        MPI_Get_processor_name(name, &len);
+        // get bounds by rank
+        int lower = lower_bound + rank * chunk;
+        int upper = lower + chunk - 1;
+        if (rank + 1 <= remainder){
+            lower = lower + rank;
+            upper = upper + rank + 1;
+        } else{
+            lower = lower + remainder;
+            upper = upper + remainder;
+        }
+        eratosthenes_worker(lower, upper);
+        // send data segment
+        for (int i = 0; i < strlen(msg); i++){
+            msg[i] = flags[lower+i];
+        }
+        sprintf(msg, "Message from process %d at %s", rank, name);
+        MPI_Send(msg, strlen(msg)+1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD);
     }
-    
+
 	// *** timing ends here ***
     std::chrono::duration<double> duration = std::chrono::system_clock::now() - start_time;
 
     // output result 
-   std::cout << "Primes: ";
-   for (int i = 0, j = maximum + 1; i < j; i++){
-      if (flags[i]){
-          std::cout << i << " ";
-      }
-   }
-   std::cout << std::endl;
+    std::cout << "Primes: ";
+    for (int i = 0, j = maximum + 1; i < j; i++){
+       if (flags[i]){
+           std::cout << i << " ";
+       }
+    }
+    std::cout << std::endl;
     
-    std::cout << "T= " << threads << ", N= " << maximum << " finished in " << duration.count() << " seconds (wall clock)." << std::endl;
+    std::cout << "N= " << maximum << " finished in " << duration.count() << " seconds (wall clock)." << std::endl;
     
+    MPI_Finalize();
     delete[] flags;
     return 0;
 }
